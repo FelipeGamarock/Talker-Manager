121a1e38492c96ef5b51ad195fee96426819a560
Felipe Gama Danta <epilefgama2@gmail.com>
2022-06-21 16:45:24 -0300

    Feat: Requisito 2: Cria o endpoitn get /talker/:id

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
 const PORT = '3000';
 
 app.get('/talker', async (_req, res) => {
     const talkers = await readTalkerFile();
     res.status(HTTP_OK_STATUS).send(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkerFile();
+  const talkerFound = talkers.find((talker) => talker.id === Number(id));
+  if (talkerFound) return res.status(HTTP_OK_STATUS).send(talkerFound);
+  return res.status(HTTP_NOTFOUND_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
